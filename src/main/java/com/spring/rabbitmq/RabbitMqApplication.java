8cdf6e42907c2255e7f2a01a471652a75c986bc9
eslam khder <eslamkhder11@gmail.com>
2022-12-14 10:20:40 +0200

    set configuration of rabbitMQ

--- src/main/java/com/spring/rabbitmq/RabbitMqApplication.java
+++ src/main/java/com/spring/rabbitmq/RabbitMqApplication.java
@@ -1,7 +1,11 @@
 package com.spring.rabbitmq;
 
+import org.springframework.amqp.rabbit.connection.AbstractConnectionFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RabbitMqApplication {
@@ -9,5 +13,4 @@ public class RabbitMqApplication {
     public static void main(String[] args) {
         SpringApplication.run(RabbitMqApplication.class, args);
     }
-
 }
