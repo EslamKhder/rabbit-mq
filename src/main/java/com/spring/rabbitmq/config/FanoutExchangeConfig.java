cc5e27412d7bd888698dcee56f3e27aab8b15061
eslam khder <eslamkhder11@gmail.com>
2022-12-14 14:40:53 +0200

    Create FanoutExchangeConfig

---
+++ src/main/java/com/spring/rabbitmq/config/FanoutExchangeConfig.java
@@ -0,0 +1,73 @@
+package com.spring.rabbitmq.config;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FanoutExchangeConfig {
+
+    @Autowired
+    private AmqpAdmin amqpAdmin;
+
+    @Value("${rabbit.fanout1.queue}")
+    private String fanoutQueue1;
+
+    @Value("${rabbit.fanout2.queue}")
+    private String fanoutQueue2;
+
+    @Value("${rabbit.fanout3.queue}")
+    private String fanoutQueue3;
+
+    @Value("${rabbit.fanout.exchange}")
+    private String exchange;
+
+    @Bean
+    Queue createFanoutQueue1(){
+        return new Queue(fanoutQueue1,true,false,false);
+    }
+
+    @Bean
+    Queue createFanoutQueue2(){
+        return new Queue(fanoutQueue2,true,false,false);
+    }
+
+    @Bean
+    Queue createFanoutQueue3(){
+        return new Queue(fanoutQueue3,true,false,false);
+    }
+
+    @Bean
+    FanoutExchange createFanoutExchange(){
+        return new FanoutExchange(exchange,true,false);
+    }
+
+    @Bean
+    public AmqpTemplate fanoutQueue(ConnectionFactory connectionFactory, MessageConverter messageConverter){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        rabbitTemplate.setExchange(exchange);
+        return rabbitTemplate;
+    }
+
+    @PostConstruct
+    public void init(){
+        amqpAdmin.declareQueue(createFanoutQueue1());
+        amqpAdmin.declareQueue(createFanoutQueue2());
+        amqpAdmin.declareQueue(createFanoutQueue3());
+
+        amqpAdmin.declareExchange(createFanoutExchange());
+    }
+
+}
