1ee8d568902d8e7d355bf405475751924a5ee2d3
eslam khder <eslamkhder11@gmail.com>
2022-12-21 15:17:51 +0200

    x-dead-letter-exchange

---
+++ src/main/java/com/spring/rabbitmq/config/DefaultDeadExchangeConfig.java
@@ -0,0 +1,53 @@
+package com.spring.rabbitmq.config;
+
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DefaultDeadExchangeConfig {
+
+    @Value("${rabbit.default.queue.dead}")
+    private String defaultQueue;
+
+    @Value("${rabbit.fanout.exchange}")
+    private String fExchange;
+
+    @Autowired
+    private AmqpAdmin amqpAdmin;
+
+    @Bean
+    Queue createQueueDead(){
+        Map<String,Object> atr = new HashMap<>();
+        atr.put("x-max-length",4);
+        //atr.put("x-message-ttl",4000);
+        //atr.put("x-overflow","reject-publish");
+        atr.put("x-dead-letter-exchange",fExchange);
+        return new Queue(defaultQueue,true,false,false,atr);
+    }
+
+    @Bean
+    public AmqpTemplate defaultQueueDead(ConnectionFactory connectionFactory, MessageConverter messageConverter){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        rabbitTemplate.setRoutingKey(defaultQueue);
+        return rabbitTemplate;
+    }
+
+    @PostConstruct
+    public void init(){
+        amqpAdmin.declareQueue(createQueueDead());
+    }
+}
